ed7ace9ff336dcac314356beef38c81dd83fc976
sulochan barakoti <sulochanbarakoti@gmail.com>
2024-07-09 22:20:32 +0300

    first commit

---
+++ app/(tabs)/_layout.jsx
@@ -0,0 +1,13 @@
+import { View, Text } from "react-native";
+import React from "react";
+import { Tabs } from "expo-router";
+
+const TabsLayout = () => {
+  return (
+    <Tabs>
+      <Tabs.Screen name="home" options={{ headerShown: false }} />
+    </Tabs>
+  );
+};
+
+export default TabsLayout;
---
+++ app/(tabs)/addItems.jsx
@@ -0,0 +1,12 @@
+import { View, Text } from "react-native";
+import React from "react";
+
+const AddItems = () => {
+  return (
+    <View>
+      <Text>AddItems</Text>
+    </View>
+  );
+};
+
+export default AddItems;
