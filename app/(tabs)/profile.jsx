51a11c56cb0997954071a88c37aa2323c0a08fd9
sulochan barakoti <sulochanbarakoti@gmail.com>
2024-07-21 14:53:52 +0300

    profile screen complete

--- app/(tabs)/profile.jsx
+++ app/(tabs)/profile.jsx
@@ -1,8 +1,9 @@
-import { View, Text, ScrollView, Image, Switch } from "react-native";
+import { View, Text, ScrollView, Image, Switch, TextInput } from "react-native";
 import React, { useState } from "react";
 import { SafeAreaView } from "react-native-safe-area-context";
 import TopBar from "../../components/topBar";
 import { images } from "../../constants";
+import CustomeButton from "../../components/customeButton";
 
 const Profile = () => {
   const [isEnabled, setIsEnabled] = useState(false);
@@ -56,8 +57,40 @@ const Profile = () => {
               <Text className="text-lg font-bold ml-2">Profile Setting</Text>
             </View>
             <View className="justify-center items-center">
-              <View className="bg-primary h-[200px] w-[95%] rounded-xl justify-center items-center space-y-2"></View>
+              <View className="bg-primary h-[180px] w-[95%] rounded-xl justify-center items-center space-y-2">
+                <View className="w-[95%] h-12 px-4 bg-gray-50 rounded-2xl focus:border-secondary items-center flex-row">
+                  <TextInput className="w-full" placeholder="First name" />
+                </View>
+                <View className="w-[95%] h-12 px-4 bg-gray-50 rounded-2xl focus:border-secondary items-center flex-row">
+                  <TextInput className="w-full" placeholder="Last name" />
+                </View>
+                <View className="w-[95%] h-12 px-4 bg-gray-50 rounded-2xl focus:border-secondary items-center flex-row">
+                  <TextInput className="w-full" placeholder="email@gmeil.com" />
+                </View>
+              </View>
             </View>
+          </View>
+          <View>
+            <View>
+              <Text className="text-lg font-bold ml-2">Security</Text>
+            </View>
+            <View className="justify-center items-center">
+              <View className="bg-primary h-[130px] w-[95%] rounded-xl justify-center items-center space-y-2">
+                <View className="w-[95%] h-12 px-4 bg-gray-50 rounded-2xl focus:border-secondary items-center flex-row">
+                  <TextInput className="w-full" placeholder="old password" />
+                </View>
+                <View className="w-[95%] h-12 px-4 bg-gray-50 rounded-2xl focus:border-secondary items-center flex-row">
+                  <TextInput className="w-full" placeholder="new password" />
+                </View>
+              </View>
+            </View>
+          </View>
+          <View className="justify-center items-center m-4">
+            <CustomeButton
+              title="Save"
+              containerStyle=" w-[50%]"
+              textStyle="text-white"
+            />
           </View>
         </View>
       </ScrollView>
